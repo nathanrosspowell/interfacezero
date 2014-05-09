79c569f9f9d9af8df37ab34e606f8c531fdda202
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-05-09 01:15:31 -0400

    looping yamls, basic bootstrap css but broken

--- js/postload.js
+++ js/postload.js
@@ -1,15 +1,52 @@
-$(window).bind("load", function() {
-    var myJson = "NOOOO";
-    $.ajax({
-        url: 'yaml/trigger_finger.yaml',
-        async: false,
-        success: function (data){
-            myJson = jsyaml.load(data);
-        }
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function tableForJson( myJ ) {
+    var x = [];
+    x.push('<div class="panel panel-default">');
+    x.push('  <div class="panel-heading">');
+    x.push('    <h3 class="panel-title">'+myJ["id"]["name"]+'</h3>');
+    x.push('  </div>');
+    x.push('  <div class="panel-body">');
+    x.push("     Race: " + myJ["id"]["race"] + "<br\>" );
+    x.push("     Occupation: " + myJ["id"]["occupation"] + "<br\>" );
+    x.push('   </div>');
+    x.push('</div>');
+    $.each(myJ["skills"],function(key,value){
+        x.push('<ul class="list-group">')
+        x.push('  <li class="list-group-item">');
+        x.push('    <span class="badge">' + value["value"] + '</span>');
+        x.push(     key );
+        x.push('  </li>');
+        x.push('  <li class="list-group-item">');
+        $.each(value["skills"],function(key,value){
+            x.push('<ul class="list-group">')
+            x.push('  <li class="list-group-item">');
+            x.push('    <span class="badge">' + value + '</span>');
+            x.push(     key );
+            x.push('  </li>');
+            x.push('</ul>');
+        });
+        x.push('  </li>');
+        x.push('</ul>');
     });
-    for(json in myJson){
-        for(objs in myJson[json]){
-            $("#interfacezero-main").append(objs + " : " + myJson[json][objs]);
-        }
-    }
+    x.forEach(function(line){
+        $("#interfacezero-main").append(line)
+    });
+
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+$(window).bind("load", function() {
+    var yamlFiles = [
+          'yaml/trigger_finger.yaml'
+        , 'yaml/derp_man.yaml'
+    ];
+    yamlFiles.forEach( function(yamlFile){
+        $.ajax({
+            url: yamlFile,
+            async: false,
+            success: function (data){
+                tableForJson(jsyaml.load(data));
+            }
+        });
+    });
 });
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
