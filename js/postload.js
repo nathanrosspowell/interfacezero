a8991d86b6698cd4703481cfa60d82a41dfb2ce2
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-05-09 14:32:30 -0400

    table for stats

--- js/postload.js
+++ js/postload.js
@@ -6,10 +6,34 @@ function tableForJson( myJ ) {
     x += '    <h3 class="panel-title"><span class="glyphicon glyphicon-user"></span>'+myJ["id"]["name"]+'</h3>';
     x += '  </div>'; // Close panel-heading
     x += '  <div class="panel-body">';
-    x += '    <div class="row">';
+    
+    
+    x += '<table class="table table-bordered table-striped">';
+    x += '  <colgroup><col class="col-xs-1"><col class="col-xs-7"></colgroup>';
+    x += '  <thead><tr>';
+    x += '    <th>XP</th>';
+    x += '    <th>Race</th>';
+    x += '    <th>Occupation</th>';
+    x += '    <th>Streetcred</th>';
+    x += '    <th>Wonded</th>';
+    x += '    <th>Exhausted</th>';
+    x += '    <th>Money</th>';
+    x += '  </tr></thead>';
+    x += '  <tbody>';
+    x += '    <tr><td>' + myJ["stats"]["xp"] + '</td></tr>';
+    x += '    <tr><td>' + myJ["id"]["race"] + '</td></tr>';
+    x += '    <tr><td>' + myJ["id"]["occupation"] + '</td></tr>';
+    x += '    <tr><td>' + myJ["stats"]["streetcreed"]["current"] + '/' + myJ["stats"]["streetcreed"]["max"] +' </td></tr>';
+    x += '    <tr><td>' + myJ["stats"]["wonded"] + '</td></tr>';
+    x += '    <tr><td>' + myJ["stats"]["exhausted"] + '</td></tr>';
+    x += '    <tr><td>' + myJ["stats"]["money"] + '</td></tr>';
+    x += '  </tbody>';
+    x += '</table>';
+        
+    
     x += '      <div class="col-md6">Race:' + myJ["id"]["race"] + '</div>';
     x += '      <div class="col-md6">Occupation:' + myJ["id"]["occupation"] + '</div>';
-    x += '    </div>';
+    
     x += '  </div>'; // Close panel-body
     x += '  <div class="well well-small">';
     $.each(myJ["skills"],function(key,value){
