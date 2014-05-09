61e085fd1f638356ff5e71bf7874b9c359b7424e
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-05-09 13:28:20 -0400

    Delete character_map.js

--- yaml/character_map.js
+++
@@ -1,5 +0,0 @@
-// Created from YAML data files.
-var character_map = {};
-character_map["derp_man"] = "Heyooo";
-character_map["trigger_finger"] = "Heyooo";
-character_map["character_map"] = "Heyooo";
