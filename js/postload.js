5d4dfdc565a42d43632f449e9d6d49e60720d585
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-05-10 17:24:22 -0400

    dynamic nav bar population

--- js/postload.js
+++ js/postload.js
@@ -1,6 +1,27 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ID helper functions
+function characterId( name ){
+    return name + "-character";
+}
+function skillsId( name ){
+    return name + "-skills";
+}
+function accordionId( name ){
+    return name + "-accordion";
+}
+function weaponsId( name ){
+    return name + "-weapons";
+}
+function campaignsId( name ){
+    return name + "-campaigns";
+}
+function notesId( name ){
+    return name + "-notes";
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function createCharacterPanel( name, myJ ){
     var x = "";
+    x += '<a class="anchor" id="' + characterId(name) +'"></a>';
     x += '<div class="panel panel-default">';
     x += '  <div class="panel-heading">';
     x += '    <h3 class="panel-title">';
@@ -9,11 +30,12 @@ function createCharacterPanel( name, myJ ){
     x += '    </h3>';
     x += '  </div>'; // Close panel-heading
     x += '  <div class="panel-body">';
-    x += '    <div class="well well-small">';
     x +=        idTable( name, myJ );
     x +=        tapTable( name, myJ );
     x +=        statsAccordion( name, myJ );
-    x += '    </div>';
+    x +=        weapons( name, myJ );
+    x +=        campaigns( name, myJ );
+    x +=        notes( name, myJ );
     x += '  </div>';
     x += '</div>';
     return x;
@@ -75,14 +97,11 @@ function tapTable( name, myJ ){
     return x;
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function accordionName( name ){
-    return name + "-accordion";
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function statsAccordion( name, myJ ){
-    var accordion = accordionName( name )
+    var accordion = accordionId( name )
     var x = "";
-    x += '<div class="panel-group" id="' + accordion + '">';
+    x += '<a class="anchor" id="' + skillsId( name ) +'"></a>';
+    x += '<div class="panel-group">';
     $.each(myJ["skills"],function(key,value){
         var anchor = name + "-" + key;
         x += statEntry( key, value, accordion, anchor );
@@ -126,12 +145,65 @@ function attributeItems( value ){
     return x;
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function tableForJson( name, myJ ) {
-    var x = createCharacterPanel( name, myJ );
-    $("#interfacezero-main").append(x)
-    $('#'+accordionName(name)).collapse("hide"); // Close that accordion
+function weapons( name, myJ ){
+    var x = "";
+    x += '<a class="anchor" id="' + weaponsId( name ) +'"></a>';
+    x += '<p>';
+    x += '  Weapons';
+    x += '</p>';
+    return x;
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function campaigns( name, myJ ){
+    var x = "";
+    x += '<a class="anchor" id="' + campaignsId( name ) +'"></a>';
+    x += '<p>';
+    x += '  campaigns';
+    x += '</p>';
+    return x;
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function notes( name, myJ ){
+    var x = "";
+    x += '<a class="anchor" id="' + notesId( name ) +'"></a>';
+    x += '<p>';
+    x += '  Notes';
+    x += '</p>';
+    return x;
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function createCharacterDropdown( name, myJ ) {
+    var shortName = myJ["id"]["name"].split(" ")[ 0 ];
+    var x = "";
+    x += '<li class="dropdown">';
+    x += '  <a href="#" class="dropdown-toggle" data-toggle="dropdown">' + shortName + '<b class="caret"></b></a>';
+    x += '  <ul class="dropdown-menu">';
+    x += '    <li><a href="#' + characterId(name)+ '">Identity</a></li>';
+    x += '    <li class="divider"></li>';
+    x += '    <li class="dropdown-header">Stats</li>';
+    x += '    <li><a href="#' + skillsId(name)+ '">Skills</a></li>';
+    x += '    <li><a href="#' + weaponsId(name)+ '">Weapons</a></li>';
+    x += '    <li class="divider"></li>';
+    x += '    <li class="dropdown-header">Back story</li>';
+    x += '    <li><a href="#' + campaignsId(name)+ '">Campaigns</a></li>';
+    x += '    <li><a href="#' + notesId(name)+ '">Notes</a></li>';
+    x += '  </ul>';
+    x += '</li>';
+    return x;
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function addHtmlForYaml( name, myJ ) {
+    // Add the html for the stats
+    var html = createCharacterPanel( name, myJ );
+    $("#interfacezero-main").append(html)
+    // Close that accordion.
+    $('#'+skillsId(name)).collapse("hide");
+    // Add the html for the navigation.
+    var dropdown = createCharacterDropdown( name, myJ );
+    $("#interfacezero-navbar").append(dropdown);
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// This is what executes
 $(window).bind("load", function() {
     var yamlFiles = [
           'trigger_finger'
@@ -142,7 +214,7 @@ $(window).bind("load", function() {
             url: 'yaml/' + yamlFile + '.yaml',
             async: false,
             success: function (data){
-                tableForJson(yamlFile,jsyaml.load(data));
+                addHtmlForYaml(yamlFile,jsyaml.load(data));
             }
         });
     });
