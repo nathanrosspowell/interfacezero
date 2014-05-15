8d4babbb5894160e016b05c96909cea6c1de0420
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-05-15 16:56:19 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,3 +36,7 @@ $(window).bind("load", function() {
     }, 500);
 });
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+$('.navbar-collapse ul li a').click(function(){ 
+
+  $('.navbar-toggle:visible').click();
+});
