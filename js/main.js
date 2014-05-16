de67807ab20b1779e78e3dbcbe30ec9fe230a006
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-05-16 10:15:56 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,18 +2,22 @@
 // This is what executes
 $(window).bind("load", function() {
     var errors = "";
+    // Loop all of the file names, found in yaml_lists.js
     playerCharacters.forEach(function(yamlFile){
         $.ajax({
             url: 'yaml/' + yamlFile + '.yaml',
             async: false,
             success: function (data){
+                // Try catch for each playrs yaml
                 try{
                     addHtmlForYaml(yamlFile,jsyaml.load(data));
                 }
                 catch(err){
+                    // If it's not the first error, add a horizontal rule
                     if (errors !== ""){
                         errors += '<hr/>';
                     }
+                    // Write out the error and callstack
                     errors += '<h4>Javascript Error</h4>';
                     errors += '<p>File: yaml/'+ yamlFile +'.yaml</p>';
                     errors += '<p>Error description:'+err.message+'</p>';
@@ -24,12 +28,18 @@ $(window).bind("load", function() {
             }
         });
     });
+    // Offset the scrolling to accomadate the fixed menu bar.
     $('body').scrollspy({offset: 60});
+    // If we no errors, hide the warning box - this say "Javascript Dependency"
     if (errors == ""){
         $('#interfacezero-warning').hide();
-    } else {
+    } 
+    // If we do have errors, write them to the warning box
+    else {
         $('#interfacezero-warning').html(errors);
     }
+    // look for an anchor and scroll to it
+    // eg 'interfacezero/#trigger_finger-skills'
     var href = window.location.hash.substring(1);
     $('html, body').animate({
         scrollTop: $( $.attr(this, href) ).offset().top
